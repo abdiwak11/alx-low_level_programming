bd482e2d5e60a60370777a011f17233c05fce4da
abdiwak11 <wakabdi1@gmail.com>
2023-02-16 09:34:07 -0800

    size of a variable

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	char d;
+	float a;
+	long int b;
+	long long int e;
+
+	printf("Size of a char: %lu\n",(unsigned long)sizeof(d));
+	printf("Size of an int: %lu\n",(unsigned long)sizeof(i));
+	printf("Size of a long int: %lu\n",(unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu\n",(unsigned long)sizeof(e));
+	printf("Size of a float: %lu\n",(unsigned long)sizeof(a));
+
+	return (0);
+
+}

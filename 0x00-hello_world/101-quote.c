836172804c770d24af36bfa59bd8faff33354a1c
abdiwak11 <wakabdi1@gmail.com>
2023-02-16 11:23:45 -0800

    my las task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- Entry point
+ *
+ * Retur: Always 0 (success)
+ */
+int main(void)
+{
+	(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19", 59);
+}

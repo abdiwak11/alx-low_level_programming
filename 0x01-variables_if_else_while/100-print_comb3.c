6682ccd84b05c87827be4939cb0fb66ce8f4ea8a
abdiwak11 <wakabdi1@gmail.com>
2023-02-17 12:57:01 -0800

    print combinatin

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i; j < 10; j++)
+		{
+			if (i = j)
+			{
+				continue;
+			}
+			putchar(48 + i);
+			putchar(48 + j);
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+
+		return (0);
+	}
+}

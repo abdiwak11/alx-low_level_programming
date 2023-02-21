a2c238e063666d578e43ec7c63803495f3110058
abdiwak11 <wakabdi1@gmail.com>
2023-02-21 13:46:10 -0800

    last number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - used to print the last line
+ * @x: parameter of a function
+ *
+ * Return: Last digit of a given num
+ */
+int print_last_digit(int x)
+{
+	int i;
+
+	i = x % 10;
+
+	return (i);
+}
+

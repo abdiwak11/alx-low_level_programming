bc226deedd08d5d1d2f82495305a15ed266f359d
abdiwak11 <wakabdi1@gmail.com>
2023-02-21 12:01:39 -0800

    lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - checks whether its lowercase alphabet or not
+ *
+ * Return: Always 0 success
+ */
+int _islower(int c);
+{
+	char t;
+
+	if (t >= 'a' && t <= 'z')
+		return (1);
+	else
+		return (0)
+}

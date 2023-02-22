57c26772d4e351a0ba7766d778aca722ac113aef
abdiwak11 <wakabdi1@gmail.com>
2023-02-22 11:28:04 -0800

    function thats prints all natural numbers from a given num

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,5 +7,58 @@
  */
 void print_to_98(int n)
 {
+	int i, k, z;
 
-
+	if (n <= 0 || n <= 98)
+	{
+		for(i = n; i <= 98; i++)
+		{
+			if (i >= 0 && i < 10)
+				_putchar(48 + i);
+			else if (i >= (-9) && i < 0)
+			{
+				k = i * (-1);
+				_putchar('-');
+				_putchar(48 + k);
+			}
+			else if (i <= (-10))
+			{
+				z = i * (-1);
+				_putchar('-');
+				_putchar((i / 10) + 48);
+				_putchar((i % 10) + 48);
+			}
+			else
+			{
+				_putchar((i / 10) + 48);
+				_putchar((i % 10) + 48);
+			}
+			
+			if (i == 98)
+				continue;
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		for (i = n; i > 98; i--)
+		{
+			if (n < 100)
+			{
+				_putchar((i / 10);
+				_putchar((i % 10);
+			}
+			else
+			{
+				_putchar((i / 100);
+				_putchar((i / 10);
+				_putchar((i % 10);
+			}
+			if (i == 98)
+				continue;
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+}

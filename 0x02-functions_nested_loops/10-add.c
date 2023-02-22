20671f8c1b7fe051b032222c9f06500a848854b9
abdiwak11 <wakabdi1@gmail.com>
2023-02-22 08:37:03 -0800

    add two integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,7 +3,8 @@
  * add - function that adds two number
  *
  * Return: returns the result
- *
+ * @i: frist parameter of the function
+ * @n: second parameter of the function
  */
 int add(int i, int n)
 {

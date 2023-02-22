75956bad4ab655476a48022bd88e53e2ff6e5bb9
abdiwak11 <wakabdi1@gmail.com>
2023-02-22 00:54:31 -0800

    printing last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,27 +8,28 @@
 int print_last_digit(int x)
 {
 	int i, n, a, j, q;
+
 	n = 0 - 10;
 	a = 1 - 2;
 
 	if (x > 10)
 	{
 		i = x % 10;
-		return (i);
+		return (ii);
 	}
 	else if (x < 10 && x >= 0)
 	{
-		return (x);
+		return (xx);
 	}
 	else if (x > n && x <= a)
 	{
 		j = x * a;
-		return (j);
+		return (jj);
 	}
 	else
 	{
 		q = (x % 10) * a;
-		return (q);
+		return (qq);
 	}
 }
 

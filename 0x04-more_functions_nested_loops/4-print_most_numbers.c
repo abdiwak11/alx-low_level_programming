fda719e3d0aa7116d65a9e20fb81a91804d1ef4f
abdiwak11 <wakabdi1@gmail.com>
2023-02-24 12:27:57 -0800

    printing nums without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers
+ * 
+ * Return: numbers
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(48 + i);
+	}
+	_putchar('\n');
+}

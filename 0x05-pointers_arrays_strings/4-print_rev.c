34faeff45271173f2f771ae49ec010faefccb23e
abdiwak11 <wakabdi1@gmail.com>
2023-02-27 12:21:47 -0800

    reversing the string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,9 +9,9 @@
 void print_rev(char *s)
 {
 	int i, j;
-	for(j = 0; s[j] != '\0'; ++j)
+	for(j = 0; s[j] != '\0'; j++)
 		;
-	for (i = j; i >= 0; i--)
+	for (i = j - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}

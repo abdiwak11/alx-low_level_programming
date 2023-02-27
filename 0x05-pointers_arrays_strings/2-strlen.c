43780f5128fc890fbeee13df8f69119786d87894
abdiwak11 <wakabdi1@gmail.com>
2023-02-27 11:13:35 -0800

    string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,14 +3,15 @@
 /**
  * _strlen - a functin that prints the length of the string
  * @s: a given string
- * Return: the length 
+ * Return: The length 
  */
 
 int _strlen(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++);
+	for (i = 0; s[i] != '\0'; i++)
+		;
 
 	return (i);
 }

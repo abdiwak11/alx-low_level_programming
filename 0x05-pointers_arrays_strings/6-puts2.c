53524de2b14d54bcea47e6678b07ad638d598817
abdiwak11 <wakabdi1@gmail.com>
2023-02-27 12:45:34 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints a string
+ * @str:  a parameter thats printed out
+ * Return:  a string
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i = 0)
+			_putchar(str[i]);
+		else
+			_putchar(str[i + 1];
+	}
+
+	_putchar('\n');
+}

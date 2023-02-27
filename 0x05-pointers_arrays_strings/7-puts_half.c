a8b18aa51b798a3b654f32ed3a12a64fc1cc4512
abdiwak11 <wakabdi1@gmail.com>
2023-02-27 13:18:32 -0800

    printing starting from half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - prints a string
+ * @str:  a parameter thats printed out
+ * Return:  a string
+ */
+
+void puts_half(char *str)
+{
+	int i, j;
+
+	for (j = 0; str[j] != '\0'; j++)
+		;
+	for (i = j / 2; i < j; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}

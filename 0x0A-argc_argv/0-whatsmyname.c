bb0a28624e03aaf8d28d57a4998535f62999e362
abdiwak11 <wakabdi1@gmail.com>
2023-03-12 10:42:43 -0700

    print the program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the program
+ *
+ * Return: Always 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
